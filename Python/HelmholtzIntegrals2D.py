bd51bf690b74bbfc83b8de75f0ea9848c463f5c4
Frank Jargstorff <frank@jargstorff.us>
2017-07-23 11:15:16 -0700

    Factored the distrete integral operator code out into a separate file (IntegralOperators2D) and adjusted calling code accordingly. This is in preparation for the Exteran Helmholtz solvers and examples by Kirkup.

---
+++ Python/HelmholtzIntegrals2D.py
@@ -0,0 +1,124 @@
+import numpy as np
+from numpy.linalg import norm
+from scipy.special import hankel1
+
+def Normal2D(pointA, pointB):                  
+    diff = pointA - pointB                          
+    len = norm(diff)                                
+    return np.array([diff[1]/len, -diff[0]/len])    
+
+def ComplexQuad(func, start, end):                                                 
+    samples = np.array([[0.980144928249, 5.061426814519E-02],                           
+                        [0.898333238707, 0.111190517227],                               
+                        [0.762766204958, 0.156853322939],                               
+                        [0.591717321248, 0.181341891689],                               
+                        [0.408282678752, 0.181341891689],                               
+                        [0.237233795042, 0.156853322939],                               
+                        [0.101666761293, 0.111190517227],                               
+                        [1.985507175123E-02, 5.061426814519E-02]], dtype=np.float32)    
+    
+    vec = end - start                                                                                                  
+    sum = 0.0                                                                                                          
+    for n in range(samples.shape[0]):                                                                                  
+        x = start + samples[n, 0] * vec                                                                                
+        sum += samples[n, 1] * func(x)                                                                                 
+    return sum * norm(vec)                                                                                         
+    
+def ComputeL(k, p, qa, qb, pOnElement):                                                                       
+    qab = qb - qa                                                                                                  
+    if pOnElement:                                                                                                 
+        if k == 0.0:                                                                                               
+            ra = norm(p - qa)                                                                                      
+            rb = norm(p - qb)                                                                                      
+            re = norm(qab)                                                                                         
+            return 0.5 / np.pi * (re - (ra * np.log(ra) + rb * np.log(rb)))                                        
+        else:                                                                                                      
+            def func(x):                                                                                           
+                R = norm(p - x)                                                                                    
+                return 0.5 / np.pi * np.log(R) + 0.25j * hankel1(0, k * R)                                         
+            return ComplexQuad(func, qa, p) + ComplexQuad(func, p, qa) \
+                 + ComputeL(0.0, p, qa, qb, True)                                                              
+    else:                                                                                                          
+        if k == 0.0:                                                                                               
+            return -0.5 / np.pi * ComplexQuad(lambda q: np.log(norm(p - q)), qa, qb)                           
+        else:                                                                                                      
+            return 0.25j * ComplexQuad(lambda q: hankel1(0, k * norm(p - q)), qa, qb)                          
+    return 0.0                                                                                                     
+                                                                                                                   
+def ComputeM(k, p, qa, qb, pOnElement):                                                                       
+    qab = qb - qa                                                                                                  
+    vecq = Normal2D(qa, qb)                                                                                    
+    if pOnElement:                                                                                                 
+        return 0.0                                                                                                 
+    else:                                                                                                          
+        if k == 0.0:                                                                                               
+            def func(x):                                                                                           
+                r = p - x                                                                                          
+                return np.dot(r, vecq) / np.dot(r, r)                                                              
+            return -0.5 / np.pi * ComplexQuad(func, qa, qb)                                                    
+        else:                                                                                                      
+            def func(x):                                                                                           
+                r = p - x                                                                                          
+                R = norm(r)                                                                                        
+                return hankel1(1, k * R) * np.dot(r, vecq) / R                                                     
+            return 0.25j * k * ComplexQuad(func, qa, qb)                                                       
+    return 0.0                                                                                                 
+                                                                                                                   
+def ComputeMt(k, p, vecp, qa, qb, pOnElement):                                                                
+    qab = qb - qa                                                                                                  
+    if pOnElement:                                                                                                 
+        return 0.0                                                                                                 
+    else:                                                                                                          
+        if k == 0.0:                                                                                               
+            def func(x):                                                                                           
+                r = p - x                                                                                          
+                return np.dot(r, vecp) / np.dot(r, r)                                                              
+            return -0.5 / np.pi * ComplexQuad(func, qa, qb)                                                    
+        else:                                                                                                      
+            def func(x):                                                                                           
+                r = p - x                                                                                          
+                R = norm(r)                                                                                        
+                return hankel1(1, k * R) * np.dot(r, vecp) / R                                                     
+            return -0.25j * k * ComplexQuad(func, qa, qb)                                                      
+                                                                                                                   
+def ComputeN(k, p, vecp, qa, qb, pOnElement):                                                                 
+    qab = qb- qa                                                                                                   
+    if pOnElement:                                                                                                 
+        ra = norm(p - qa)                                                                                          
+        rb = norm(p - qb)                                                                                          
+        re = norm(qab)                                                                                             
+        if k == 0.0:                                                                                               
+            return -(1.0 / ra + 1.0 / rb) / (re * 2.0 * np.pi) * re                                                
+        else:                                                                                                      
+            vecq = Normal2D(qa, qb)                                                                            
+            k2 = k * k                                                                                             
+            def func(x):                                                                                           
+                r = p - x                                                                                          
+                R2 = np.dot(r, r)                                                                                  
+                R = np.sqrt(R2)                                                                                    
+                drdudrdn = -np.dot(r, vecq) * np.dot(r, vecp) / R2                                                 
+                dpnu = np.dot(vecp, vecq)                                                                          
+                c1 =  0.25j * k / R * hankel1(1, k * R)                                  - 0.5 / (np.pi * R2)      
+                c2 =  0.50j * k / R * hankel1(1, k * R) - 0.25j * k2 * hankel1(0, k * R) - 1.0 / (np.pi * R2)      
+                c3 = -0.25  * k2 * np.log(R) / np.pi                                                               
+                return c1 * dpnu + c2 * drdudrdn + c3                                                              
+            return ComputeN(0.0, p, vecp, qa, qb, True) - 0.5 * k2 * ComputeL(0.0, p, qa, qb, True) \
+                 + ComplexQuad(func, qa, p) + ComplexQuad(func, p, qb)                                     
+    else:                                                                                                          
+        sum = 0.0j                                                                                                 
+        vecq = Normal2D(qa, qb)                                                                                
+        un = np.dot(vecp, vecq)                                                                                    
+        if k == 0.0:                                                                                               
+            def func(x):                                                                                           
+                r = p - x                                                                                          
+                R2 = np.dot(r, r)                                                                                  
+                drdudrdn = -np.dot(r, vecq) * np.dot(r, vecp) / R2                                                 
+                return (un + 2.0 * drdudrdn) / R2                                                                  
+            return 0.5 / np.pi * ComplexQuad(func, qa, qb)                                                     
+        else:                                                                                                      
+            def func(x):                                                                                           
+                r = p - x                                                                                          
+                drdudrdn = -np.dot(r, vecq) * np.dot(r, vecp) / np.dot(r, r)                                       
+                R = norm(r)                                                                                        
+                return hankel1(1, k * R) / R * (un + 2.0 * drdudrdn) - k * hankel1(0, k * R) * drdudrdn            
+            return 0.25j * k * ComplexQuad(func, qa, qb)                                                       
--- Python/InteriorHelmholtzSolver2D.py
+++ Python/InteriorHelmholtzSolver2D.py
@@ -1,3 +1,5 @@
+from HelmholtzIntegrals2D import *
+
 import numpy as np
 from numpy.linalg import norm
 from scipy.special import hankel1
@@ -67,132 +69,6 @@ class InteriorHelmholtzSolver2D(object):
         return 2.0 * np.pi * f / self.c
 
     @classmethod
-    def Normal2D(cls, pointA, pointB):
-        diff = pointA - pointB
-        len = norm(diff)
-        return np.array([diff[1]/len, -diff[0]/len])
-    
-    @classmethod
-    def ComplexQuad(cls, func, start, end):
-        samples = np.array([[0.980144928249, 5.061426814519E-02], 
-                            [0.898333238707, 0.111190517227], 
-                            [0.762766204958, 0.156853322939], 
-                            [0.591717321248, 0.181341891689], 
-                            [0.408282678752, 0.181341891689],
-                            [0.237233795042, 0.156853322939], 
-                            [0.101666761293, 0.111190517227],
-                            [1.985507175123E-02, 5.061426814519E-02]], dtype=np.float32)
-        vec = end - start
-        sum = 0.0
-        for n in range(samples.shape[0]):
-            x = start + samples[n, 0] * vec
-            sum += samples[n, 1] * func(x)
-        return sum * norm(vec)
-    
-    @classmethod
-    def ComputeL(cls, k, p, qa, qb, pOnElement):
-        qab = qb - qa
-        if pOnElement:
-            if k == 0.0:
-                ra = norm(p - qa)
-                rb = norm(p - qb)
-                re = norm(qab)
-                return 0.5 / np.pi * (re - (ra * np.log(ra) + rb * np.log(rb)))
-            else:
-                def func(x):
-                    R = norm(p - x)
-                    return 0.5 / np.pi * np.log(R) + 0.25j * hankel1(0, k * R)
-                return cls.ComplexQuad(func, qa, p)  + cls.ComplexQuad(func, p, qa) \
-                     + cls.ComputeL(0.0, p, qa, qb, True)
-        else:
-            if k == 0.0:
-                return -0.5 / np.pi * cls.ComplexQuad(lambda q: np.log(norm(p - q)), qa, qb)
-            else:
-                return 0.25j * cls.ComplexQuad(lambda q: hankel1(0, k * norm(p - q)), qa, qb)
-        return 0.0
-        
-    @classmethod
-    def ComputeM(cls, k, p, qa, qb, pOnElement):
-        qab = qb - qa
-        vecq = cls.Normal2D(qa, qb)
-        if pOnElement:
-            return 0.0
-        else:
-            if k == 0.0:
-                def func(x):
-                    r = p - x
-                    return np.dot(r, vecq) / np.dot(r, r)
-                return -0.5 / np.pi * cls.ComplexQuad(func, qa, qb)
-            else:
-                def func(x):
-                    r = p - x
-                    R = norm(r)
-                    return hankel1(1, k * R) * np.dot(r, vecq) / R
-                return 0.25j * k * cls.ComplexQuad(func, qa, qb)
-        return 0.0
-
-    @classmethod
-    def ComputeMt(cls, k, p, vecp, qa, qb, pOnElement):
-        qab = qb - qa
-        if pOnElement:
-            return 0.0
-        else:
-            if k == 0.0:
-                def func(x):
-                    r = p - x
-                    return np.dot(r, vecp) / np.dot(r, r)
-                return -0.5 / np.pi * cls.ComplexQuad(func, qa, qb)
-            else:
-                def func(x):
-                    r = p - x
-                    R = norm(r)
-                    return hankel1(1, k * R) * np.dot(r, vecp) / R
-                return -0.25j * k * cls.ComplexQuad(func, qa, qb)
-    
-    @classmethod
-    def ComputeN(cls, k, p, vecp, qa, qb, pOnElement):
-        qab = qb- qa
-        if pOnElement:
-            ra = norm(p - qa)
-            rb = norm(p - qb)
-            re = norm(qab)
-            if k == 0.0:
-                return -(1.0 / ra + 1.0 / rb) / (re * 2.0 * np.pi) * re
-            else:
-                vecq = cls.Normal2D(qa, qb)
-                k2 = k * k
-                def func(x):
-                    r = p - x
-                    R2 = np.dot(r, r)
-                    R = np.sqrt(R2)
-                    drdudrdn = -np.dot(r, vecq) * np.dot(r, vecp) / R2
-                    dpnu = np.dot(vecp, vecq)
-                    c1 =  0.25j * k / R * hankel1(1, k * R)                                  - 0.5 / (np.pi * R2)
-                    c2 =  0.50j * k / R * hankel1(1, k * R) - 0.25j * k2 * hankel1(0, k * R) - 1.0 / (np.pi * R2)
-                    c3 = -0.25  * k2 * np.log(R) / np.pi
-                    return c1 * dpnu + c2 * drdudrdn + c3
-                return cls.ComputeN(0.0, p, vecp, qa, qb, True) - 0.5 * k2 * cls.ComputeL(0.0, p, qa, qb, True) \
-                     + cls.ComplexQuad(func, qa, p) + cls.ComplexQuad(func, p, qb)
-        else:
-            sum = 0.0j
-            vecq = cls.Normal2D(qa, qb)
-            un = np.dot(vecp, vecq)
-            if k == 0.0:
-                def func(x):
-                    r = p - x
-                    R2 = np.dot(r, r)
-                    drdudrdn = -np.dot(r, vecq) * np.dot(r, vecp) / R2
-                    return (un + 2.0 * drdudrdn) / R2
-                return 0.5 / np.pi * cls.ComplexQuad(func, qa, qb)
-            else:
-                def func(x):
-                    r = p - x
-                    drdudrdn = -np.dot(r, vecq) * np.dot(r, vecp) / np.dot(r, r)
-                    R = norm(r)
-                    return hankel1(1, k * R) / R * (un + 2.0 * drdudrdn) - k * hankel1(0, k * R) * drdudrdn 
-                return 0.25j * k * cls.ComplexQuad(func, qa, qb)   
-        
-    @classmethod
     def SolveLinearEquation(cls, Ai, Bi, ci, alpha, beta, f):
         A = np.copy(Ai)
         B = np.copy(Bi)
@@ -245,15 +121,15 @@ class InteriorHelmholtzSolver2D(object):
             pb = self.aVertex[self.aElement[i, 1]]
             pab = pb - pa
             center = 0.5 * (pa + pb)
-            centerNormal = self.Normal2D(pa, pb)
+            centerNormal = Normal2D(pa, pb)
             for j in range(self.aElement.shape[0]):
                 qa = self.aVertex[self.aElement[j, 0]]
                 qb = self.aVertex[self.aElement[j, 1]]
 
-                elementL  = self.ComputeL(k, center, qa, qb, i==j)
-                elementM  = self.ComputeM(k, center, qa, qb, i==j)
-                elementMt = self.ComputeMt(k, center, centerNormal, qa, qb, i==j)
-                elementN  = self.ComputeN(k, center, centerNormal, qa, qb, i==j)
+                elementL  = ComputeL(k, center, qa, qb, i==j)
+                elementM  = ComputeM(k, center, qa, qb, i==j)
+                elementMt = ComputeMt(k, center, centerNormal, qa, qb, i==j)
+                elementN  = ComputeN(k, center, centerNormal, qa, qb, i==j)
                 
                 A[i, j] = elementL + mu * elementMt
                 B[i, j] = elementM + mu * elementN
@@ -291,8 +167,8 @@ class InteriorHelmholtzSolver2D(object):
                 qa = self.aVertex[self.aElement[j, 0]]
                 qb = self.aVertex[self.aElement[j, 1]]
 
-                elementL  = self.ComputeL(solution.k, p, qa, qb, False)
-                elementM  = self.ComputeM(solution.k, p, qa, qb, False)
+                elementL  = ComputeL(solution.k, p, qa, qb, False)
+                elementM  = ComputeM(solution.k, p, qa, qb, False)
 
                 sum += elementL * solution.aV[j] - elementM * solution.aPhi[j]
             aResult[i] = sum
